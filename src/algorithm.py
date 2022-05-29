91320dccb56bcc80c8c81a1cc1c80250c476676d
harsh <h.munshi@oxolo.com>
2022-05-29 22:28:09 +0200

    [new] basic tree formation for score search

--- src/algorithm.py
+++ src/algorithm.py
@@ -6,27 +6,100 @@ class State:
     def __init__(self, start: str) -> None:
         self.state = 'N'*9
         self.start = start
+        self.score = 0
     
     def check_terminating_condition(self):
-        raise NotImplementedError()
+        if 'N' in self.state:
+            return False
+        else:
+            return True
     
-    def check_winning_positions():
-        raise NotImplementedError()
+    def check_winning_positions(self, player: str):
+        if player == "maxi":
+            score = 10
+        else:
+            score = -10
+        # check winning positions in case it is achieved before the state is complete
+        # 1. check horizonal positions
+        # sample = 'N N N N N N N N N'
+        if (self.state[0] == self.state[1] and self.state[1] == self.state[2] and self.state[0] != 'N' and self.state[2] != 'N'):
+            return score
+        elif (self.state[3] == self.state[4] and self.state[4] == self.state[5] and self.state[3] != 'N' and self.state[5] != 'N'):
+            return score
+        elif (self.state[6] == self.state[7] and self.state[7] == self.state[8] and self.state[6] != 'N' and self.state[8] != 'N'):
+            return score
+        
+        # 2. check vertical positions
+        if (self.state[0] == self.state[3] and self.state[3] == self.state[6] and self.state[0] != 'N' and self.state[3] != 'N'):
+            return score
+        elif (self.state[1] == self.state[4] and self.state[4] == self.state[7] and self.state[1] != 'N' and self.state[7] != 'N'):
+            return score 
+        elif (self.state[2] == self.state[5] and self.state[5] == self.state[8] and self.state[2] != 'N' and self.state[8] != 'N'):
+            return score
+        
+        # 3. Check diagonals
+        if (self.state[0] == self.state[4] and self.state[4] == self.state[8] and self.state[0] != 'N' and self.state[8] != 'N'):
+            return 10
+        elif (self.state[2] == self.state[4] and self.state[4] == self.state[6] and self.state[2] != 'N' and self.state[6] != 'N'):
+            return 10
+        
+        # 4. if not anything
+        return 0
+
+
     
-    def find_next_child(self):
-        raise NotImplementedError()
+    def find_next_children(self, player: str):
+        s = list(self.state)
+        children = []
+        for i in range(9):
+            if s[i] == 'N':
+                if player == "mini":
+                    s[i] = "O"
+                elif player == "maxi":
+                    s[i] = "X"
+                children.append("".join(s))
+                s[i] = "N"
+        
+        return children
     
-    def next_best_move(self):
-        # This is the minimax algorithm 
-        raise NotImplementedError()
-    
+    def next_best_move(self, player: str):
+        # This is the minimax algorithm
+        if player == "maxi":
+            # maximize the score
+            pass
+        
+        else:
+            # check if board is either full or someone has won
+            score = []
+            tree = []
+            while(not self.check_terminating_condition()):
+                tree = self.find_next_children("mini")
+                print(tree)
+                for child in tree:
+                    # check if the children have hit the winning condition
+                    print(child)
+                    self.state = child
+                    # expand the child if winning position is not true
+                    if (not self.check_winning_positions("mini")):
+                        tree.append(self.find_next_children("mini"))     
+                    else: 
+                        tree.append(self.find_next_children("mini"))
     
 
 if __name__=="__main__":
     x = input(f"Welcome to tic-tac-toe. Select a player to start, H for human or C for computer: ")
-    if str(x) != "H" or str(x) != "C":
-        raise TypeError(f"Please select appropriate player")
+    # if str(x) != "H" or str(x) != "C":
+    #     raise TypeError(f"Please select appropriate player")
 
     # Initialize the board
     board = State(x)
-    terminal_visualisation(board.state)
+    if x == "H":
+        x = input(f"Input your move as [x+y] coordinate: ")
+        # [i,j] ==> The index for the string will be i+j
+        s = list(board.state)
+        s[int(x)] = "X"
+        board.state = "".join(s)
+
+        # compute the next best move
+        # while (not board.check_terminating_condition()):
+        board.next_best_move("mini")
